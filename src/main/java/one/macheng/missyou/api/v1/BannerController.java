6360e7190f24ce9cc4306c0f080397d7a4f7bde7
macheng2017 <gos360@163.com>
2020-03-02 21:34:12 +0800

    feat:如果每个方法都需要ResponseBody 可以提取到类上 如果@Controller 和 @ResponseBody 可以合为 @RestController

--- src/main/java/one/macheng/missyou/api/v1/BannerController.java
+++ src/main/java/one/macheng/missyou/api/v1/BannerController.java
@@ -1,16 +1,15 @@
 package one.macheng.missyou.api.v1;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class BannerController {
     //    @GetMapping("/test")
     @RequestMapping(value = "/test", method = {RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
+
+    // 如果每个方法都需要ResponseBody 可以提取到类上
+    // 如果@Controller 和 @ResponseBody 可以合为 @RestController
     public String test() {
         return "hello 世界1234567";
     }
