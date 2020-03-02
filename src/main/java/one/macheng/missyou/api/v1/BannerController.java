a0f172b3caa54a8e5cc81454d33d0a076a63872f
macheng2017 <gos360@163.com>
2020-03-02 21:26:41 +0800

    feat: 几个常用的注解用法 @RequestMapping @PostMapping @GetMapping ...

--- src/main/java/one/macheng/missyou/api/v1/BannerController.java
+++ src/main/java/one/macheng/missyou/api/v1/BannerController.java
@@ -2,19 +2,16 @@ package one.macheng.missyou.api.v1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class BannerController {
-    @GetMapping("/test")
+    //    @GetMapping("/test")
+    @RequestMapping(value = "/test", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    // 直接使用这个注解可以直接使用return返回字符串的方式,写入到response当中
-    // 并且解决了字符串编码问题,真方便
     public String test() {
-        return "hello 世界";
-    }// hello ??
+        return "hello 世界1234567";
+    }
 }
