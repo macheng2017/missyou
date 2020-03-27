080a07071e587a775e7cb88593265f420e0cde3d
cheng ma <macheng2025@gmail.com>
2020-03-27 17:11:17 +0800

    feat:SpringBoot最基础的编程模式@Component+@Autowired

--- src/main/java/one/macheng/missyou/api/v1/BannerController.java
+++ src/main/java/one/macheng/missyou/api/v1/BannerController.java
@@ -1,15 +1,22 @@
 package one.macheng.missyou.api.v1;
 
+import one.macheng.missyou.sample.hero.Diana;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/v1/banner/")
 //用一个通用的method @RequestMapping还可以用作前缀
 // 使用http://localhost:8080/v1/banner/test访问,这个就是restFul api 的自描述性
 public class BannerController {
+    @Autowired
+    private Diana diana;
+
     @GetMapping("/test")
     public String test() {
+        diana.q();
         return "hello 世界1234567";
     }
 }
---
+++ src/main/java/one/macheng/missyou/sample/hero/Diana.java
@@ -0,0 +1,10 @@
+package one.macheng.missyou.sample.hero;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Diana {
+    public void q(){
+        System.out.println("Diana Q");
+    }
+}
